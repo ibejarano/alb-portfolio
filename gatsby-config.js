4c9de7fb5c89eb9adf7fce5069246fe7bc450acd
Ignacio Bejarano <ignbejarano@gmail.com>
2021-04-28 19:25:55 -0300

    added hero background

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,12 @@ module.exports = {
     author: `@ibejarano`,
   },
   plugins: [
+    `gatsby-plugin-image`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,18 +18,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ana-laura-blanco-portfolio`,
+        short_name: `alb-portfolio`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
   ],
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,12 +1,35 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { getImage, GatsbyImage } from "gatsby-plugin-image"
 
-import "../styles/hero.css"
+import { convertToBgImage } from "gbimage-bridge"
+import BackgroundImage from "gatsby-background-image"
 
-const Hero = () => (
-  <div className="hero-container">
-    <h1>Titulo</h1>
-    <h2>Subtitulo</h2>
-  </div>
-)
+const Hero = () => {
+  const { placeholderImage } = useStaticQuery(
+    graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "ana-background.png" }) {
+          childImageSharp {
+            gatsbyImageData(
+              placeholder: BLURRED
+            )
+          }
+        }
+      }
+    `
+  )
+
+  const image = getImage(placeholderImage)
+  const bgImage = convertToBgImage(image)
+
+  return (
+    <BackgroundImage Tag="section" {...bgImage} preserveStackingContext>
+      <div style={{ minHeight: 1000, minWidth: 1000 }}>
+        {/* <GatsbyImage image={image} alt={"testimage"} /> */}
+      </div>
+    </BackgroundImage>
+  )
+}
 
 export default Hero
